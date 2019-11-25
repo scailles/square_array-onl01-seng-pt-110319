7f823d05c0934921a46b20564f9f79465d7673c8
celebrated-integer-2023 <scailles23@gmail.com>
2019-11-25 08:33:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,8 @@ numbers=[1,2,3]
 def square_array(numbers)
   new_numbers=[]
   numbers.each do |num|
-   new_numbers<<(num**2)
+   new_numbers<<(#{num}**2)
   end
   return
-  new_numbers
-  square_array(new_numbers)
+ new_numbers
 end
